4be4db25bb8dab8b2820dede92310fabab8946f4
NitinSangwan-584 <nitinsangwan363@gmail.com>
2025-03-26 19:02:38 +0530

    story done

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,40 +7,7 @@ const navbarbtn = () => {
     document.body.classList.toggle("overflow-hidden")
 }
 //------------------------------------------------slider---------------------------------------------
-$('.slider-content').slick({
-    dots: false,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 700,
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 8,
-    slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 5,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-});
+
 
 //----------------------------------------------back to top------------------------------------------------
 const myButton = document.getElementById("myBtn");
