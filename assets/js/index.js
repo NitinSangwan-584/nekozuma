080f8a66e3d47844b4b7ad04fae88ec4e5c84162
NitinSangwan-584 <nitinsangwan363@gmail.com>
2025-03-28 12:47:39 +0530

    add theme btn

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,27 @@ const navbarbtn = () => {
     document.getElementById("rotates2").classList.toggle("rotate-45")
     document.body.classList.toggle("overflow-hidden")
 }
-//------------------------------------------------slider---------------------------------------------
+//------------------------------------------------theme---------------------------------------------
 
+// Function to switch between light and dark themes
+function switchTheme() {
+    const body = document.body;
+    const themeToggleButton = document.getElementById('theme-toggle');
+    const sunContainer = document.getElementById('sun-container');
+    const moonContainer = document.getElementById('moon-container');
 
+    // Toggle the dark class on the body
+    body.classList.toggle('dark');
+
+    // Toggle the visibility of the sun and moon icons
+    if (body.classList.contains('dark')) {
+        sunContainer.style.display = 'none'; // Hide sun icon
+        moonContainer.style.display = 'block'; // Show moon icon
+    } else {
+        sunContainer.style.display = 'block'; // Show sun icon
+        moonContainer.style.display = 'none'; // Hide moon icon
+    }
+}
 //----------------------------------------------back to top------------------------------------------------
 const myButton = document.getElementById("myBtn");
 
